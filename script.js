8114490c6f813e0e26b8e41bc14724bcf19a14ed
Denis <example@gmail.com>
2022-05-04 18:51:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const storeName = 'Artur';
+const storeDecription = {
+    budget: 10000,
+    employees:['Anna','Piter','Elena'],
+    products:{
+        'bred':20,
+        'milk':80
+    },
+    open: true
+}
+
+console.log(storeDescription.products.bred);
